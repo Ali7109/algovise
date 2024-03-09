6518d5403ea7a8e213c333ca7fcbb324e0913b89
Ali Amin <aliamin745@gmail.com>
2024-03-09 12:48:18 -0500

    Fixed dfs path bug

--- src/PathFindingVisualizer/PathFindingVisualizer.jsx
+++ src/PathFindingVisualizer/PathFindingVisualizer.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Node from './Node/Node'
 import './PathFindingVisualizer.css'
-import { runDijkstra } from './algorithms/dijkstra'
-import { runBfs } from './algorithms/bfs'
-import { runDfs } from './algorithms/dfs'
+import Header from './Header/Header'
 
 const PathFindingVisualizer = () => {
 
@@ -11,55 +9,8 @@ const PathFindingVisualizer = () => {
   const [start, setStart] = useState([10, 5])
   const [end, setEnd] = useState([10, 25])
   const [isMouseDown, setIsMouseDown] = useState(false)
-  const [running, setRunning] = useState(false)
 
-  const alogirthms = [
-    {
-      name: 'Dijkstra',
-      run: () => runDijkstra(start, end, grid, setGrid)
-    },
-    {
-      name: 'A*',
-      run: () => console.log('A*')
-    },
-    {
-      name: 'Depth-First Search',
-      run: () => runAlgorithm(runDfs)
-    },
-    {
-      name: 'Breadth-First Search',
-      run: () => runAlgorithm(runBfs)
-    },
-    {
-      name: 'Clear',
-      run: () => clearGrid()
-    }
-  ]
-
-  const runAlgorithm = async (algorithm) => {
-      clearGrid()
-      setRunning(true)
-      const visitedNodesInOrder = await algorithm(start, end, grid, setGrid); // Run the algorithm
-      setRunning(false)
-      // animateTraversal(visitedNodesInOrder); // Animate the traversal using visitedNodesInOrder
-  };
-
-  // const animateTraversal = (visitedNodesInOrder) => {
-  //     for (let i = 0; i < visitedNodesInOrder.length; i++) {
-  //         setTimeout(() => {
-  //             const node = visitedNodesInOrder[i];
-  //             const newGrid = grid.slice();
-  //             const newNode = {
-  //                 ...node,
-  //                 isActive: true,
-  //             };
-  //             newGrid[node.row][node.col] = newNode;
-  //             setGrid(newGrid);
-  //           }, i * 100); // Adjust the animation speed
-  //     }
-  // };
-
-
+  
   const createNode = (row, col) => {
     const node = {
           row,
@@ -85,32 +36,14 @@ const PathFindingVisualizer = () => {
     }
     return grid
   }
-  const clearGrid = () => {
-    const newGrid = grid.slice()
-    for (let row = 0; row < 20; row++) {
-      for (let col = 0; col < 30; col++) {
-        const node = newGrid[row][col]
-        newGrid[row][col] = {
-          ...node,
-          isActive: false,
-          aBlock: false,
-          path: false
-        }
-      }
-    }
-    setGrid(newGrid)
-  }
+
   useEffect(() => {
     setGrid(getInitialGrid())
   }, [])
 
   return (
     <div className='container'>
-      <div className="header">
-        {alogirthms.map((algo, idx) => (
-          <button key={idx} disabled={running} onClick={algo.run}>{algo.name}</button>
-        ))}
-      </div>
+     <Header start={start} end={end} grid={grid} setGrid={setGrid} />
       <div className="grid-container" >
         {grid.map((row, rowIdx) => {
           return (
